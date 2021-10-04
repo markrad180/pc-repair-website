2c66a61ccb106ad996863f709b918e6452d1594d
Mark Rad <33667101+markrad180@users.noreply.github.com>
2021-10-03 20:08:23 -0400

    updated navbar to use burger on small screens (#6)

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  @ViewChild('navBurger', {static: true}) navBurger!: ElementRef;
+  @ViewChild('navMenu', {static: true}) navMenu!: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
     let something = '';
+  }
+
+  toggleNavbar() {
+    this.navBurger.nativeElement.classList.toggle('is-active');
+    this.navMenu.nativeElement.classList.toggle('is-active');
   }
 
 }
